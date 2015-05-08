e685a73f4e01eccb7959f9f7bf527b01dfa95afe
Velsu <riftmage50@gmail.com>
2015-05-09 00:42:03 +0200

    Add Name to users model and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :links do
     member do
-      put "Like", to: "links#upvote"
-      put "Dislike", to: "links#downvote"
+      put "like", to: "links#upvote"
+      put "dislike", to: "links#downvote"
     end
   end
 
